907bd397bcbad309b88cec764d563117fa84a3c7
ItaiF <Itaifri@campus.technion.ac.il>
2020-03-30 03:08:25 +0300

    With drag and scale
    FIXED: now only drags when enabled
    
    need to fix the explosion during drag

--- Assets/DragObject1.cs
+++ Assets/DragObject1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DragObject1 : MonoBehaviour
 {
@@ -8,6 +9,13 @@ public class DragObject1 : MonoBehaviour
     Vector3 dist;
     float posX;
     float posY;
+    Toggle enableToggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enableToggle = GameObject.Find("Toggle").GetComponent<Toggle>(); // when on -> disable
+    }
 
     void OnMouseDown()
     {
@@ -17,14 +25,25 @@ public class DragObject1 : MonoBehaviour
 
     }
 
+    void OnMouseEnter()
+    {
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+
+    }
+
     void OnMouseDrag()
     {
-        Vector3 curPos =
+        if (!enableToggle.isOn)
+        {
+            Vector3 curPos =
          new Vector3(Input.mousePosition.x - posX,
                      Input.mousePosition.y - posY, dist.z);
 
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
-        transform.position = worldPos;
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+            transform.position = worldPos;
+        }
     }
 }
 
