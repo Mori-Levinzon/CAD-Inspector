3444d2cb08e538a89cab9856370d7a4d5396095f
ItaiF <Itaifri@campus.technion.ac.il>
2020-04-07 05:28:51 +0300

    Added CrossCut
    Some bugs and feature parts are still left

---
+++ Assets/Invert.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Invert : MonoBehaviour
+{
+
+    public Slider xRot;
+
+    public void InvertXAxis()
+    {
+        if (xRot.value < 0)
+        {
+            xRot.value += 180;
+        }
+        else
+        {
+            xRot.value -= 180;
+        }
+    }
+    
+}
