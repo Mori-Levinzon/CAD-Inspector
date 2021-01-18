be8e7585e9704ecc36c73264ddeea57f4c1c9bc6
Mori Levinzon <26859371+Mori-Levinzon@users.noreply.github.com>
2021-01-18 04:49:36 +0200

    cross section fix to support colored objects

--- Assets/ChangeShaderForCross.cs
+++ Assets/ChangeShaderForCross.cs
@@ -7,6 +7,12 @@ public class ChangeShaderForCross : MonoBehaviour
 
     public Shader shaderCross;
 
+    static MeshRenderer[] CubeMeshRenders;
+
+    static Shader[] cubeShaders;
+
+    public static bool isCrossPanel = false;
+
     void Start()
     {
         //shaderCross = Shader.Find("OnePlaneBSP");
@@ -40,6 +46,52 @@ public class ChangeShaderForCross : MonoBehaviour
             i++;
         }
 
+    }
+
+    public void CrossCutbuttonPressed()
+    {
+        if (!isCrossPanel)
+        {
+            isCrossPanel = true;
+
+            cubeShaders = new Shader[GetComponentsInChildren<MeshRenderer>().Length];
+            Debug.Log("CrossCutbuttonPressed GetComponentsInChildren<MeshRenderer>().Length: " + GetComponentsInChildren<MeshRenderer>().Length);
+
+            int i = 0;
+            foreach (var item in GetComponentsInChildren<MeshRenderer>())
+            {
+                cubeShaders[i] = item.material.shader;
+                Debug.Log("CrossCutbuttonPressed in cubeShaders"+i+ ": "+ cubeShaders[i]);
+                if (i > 0)
+                {
+                    item.material.shader = shaderCross;
+                }
+                i++;
+            }
+            Debug.Log("CrossCutbuttonPressed out i: "+ i);
+
+
+        }
+        else
+        {
+            isCrossPanel = false;
+
+            int i = 0;
+            foreach (var item in GetComponentsInChildren<MeshRenderer>())
+            {
+                if (i > 0)
+                {
+                    item.material.shader = cubeShaders[i];
+                }
+                Debug.Log("CrossCutbuttonPressed in cubeShaders" + i + ": " + item.material.shader);
+
+
+
+                i++;
+            }
+            Debug.Log("CrossCutbuttonPressed out i: " + i);
+
+        }
     }
 
 }
