66465c9592788a777ab10ad309d0c13608d5684f
Mori Levinzon <26859371+Mori-Levinzon@users.noreply.github.com>
2021-01-19 12:07:54 +0200

    cross Section in 3 dimensions fixed

--- Assets/QuadMoveAsCube.cs
+++ Assets/QuadMoveAsCube.cs
@@ -8,9 +8,10 @@ public class QuadMoveAsCube : MonoBehaviour
     Vector3 offset = Vector3.zero;
     Vector3 localPos = Vector3.zero;
     Vector3 originalPos = Vector3.zero;
+    Quaternion prevCubeRot = Quaternion.identity;
     Vector3 prevCubePos = Vector3.zero;
     Vector3 prevCubeScale = Vector3.zero;
-    static bool isLoadedObject = true;
+    static bool isLoadedObject = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,31 +22,47 @@ public class QuadMoveAsCube : MonoBehaviour
     {
         if (isLoadedObject)
         {
-            prevCubePos = cube.transform.position;
+            prevCubePos = cube.transform.localPosition;
             prevCubeScale = cube.transform.localScale;
             isLoadedObject = false;
         }
     }
     void Update()
     {
-        if (GameObject.Find("LoadedObj #1") != null)
+        cube = GameObject.Find("Cube #1");
+        if (cube != null)
         {
+            isLoadedObject = true;
             setPosAndScale();
         }
         else
         {
-            isLoadedObject = true;
+            isLoadedObject = false;
         }
-        if (cube.transform.position != prevCubePos)// if cube moved
+
+        if (cube.transform.localPosition != prevCubePos)// if cube moved
         {
-            transform.position = transform.position + cube.transform.position - prevCubePos;
-            prevCubePos = cube.transform.position;
+            Debug.Log("QuadMoveAsCube position before: " + prevCubePos);
+            Debug.Log("QuadMoveAsCube position after: " + cube.transform.position);
+            //transform.localPosition = transform.localPosition + cube.transform.localPosition - prevCubePos;
+            transform.localPosition = cube.transform.localPosition;
+            prevCubePos = cube.transform.localPosition;
         }
         if (cube.transform.localScale != prevCubeScale)
         {
-            transform.localScale = transform.localScale * (cube.transform.localScale.x / prevCubeScale.x); // im counting on the fact that all axis are scaling the same
+            Debug.Log("QuadMoveAsCube localScale before: " + prevCubeScale);
+            Debug.Log("QuadMoveAsCube localScale after: " + cube.transform.localScale);
+            //transform.localScale = transform.localScale * (cube.transform.localScale.x / prevCubeScale.x); // im counting on the fact that all axis are scaling the same
+            transform.localScale = cube.transform.localScale ; // im counting on the fact that all axis are scaling the same
             prevCubeScale = cube.transform.localScale;
         }
-        
+        if (!(cube.transform.rotation == prevCubeRot))
+        {
+            Debug.Log("QuadMoveAsCube rotate before: " + prevCubeRot);
+            Debug.Log("QuadMoveAsCube rotate after: " + cube.transform.rotation);
+            transform.rotation = cube.transform.rotation;
+            prevCubeRot = cube.transform.rotation;
+        }
+
     }
 }
